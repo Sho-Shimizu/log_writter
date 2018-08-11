407d2e9f444c51bdbf0155a8eac3a4cb64f0de6d
清水翔 <maxiksan1@gmail.com>
2018-08-12 00:10:01 +0900

    update LogName, script

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var samples = require('./routes/samples');   
 
 var app = express();
 
@@ -22,7 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/samples', samples);  
+
+// ここから追加分
+var maxy = require('./routes/maxy');
+app.use('/maxy', maxy);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,3 +43,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
---
+++ routes/maxy.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var log4js = require('log4js');
+log4js.configure({
+  appenders: { Maxy: { type: 'file', filename: 'Maxy.log' } },
+  categories: { default: { appenders: ['Maxy'], level: 'info' } }
+});
+var logger = log4js.getLogger('Maxy');
+
+
+/* サンプルAPI① 
+ * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
+ * JSON形式で文字列を返す。
+ */
+router.post('/', function(req, res, next) {
+  var param = req.body.message;
+  // res.header('Content-Type', 'application/text; charset=utf-8')
+  res.send(param);
+  logger.info(param);
+});
+
+/* サンプルAPI① 
+ * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
+ * JSON形式で文字列を返す。
+ */
+router.post('/says', function(req, res, next) {
+  var message = req.body.message;
+  // res.header('Content-Type', 'application/text; charset=utf-8')
+  res.send(message);
+  logger.info(message);
+});
+
+/* サンプルAPI① 
+ * http://localhost:3000/samples にGETメソッドのリクエストを投げると、
+ * JSON形式で文字列を返す。
+ */
+router.post('/notification', function(req, res, next) {
+  var message = req.body.message;
+  // res.header('Content-Type', 'application/text; charset=utf-8')
+  res.send(message);
+  logger.info(message);
+});
+
+
+module.exports = router;
